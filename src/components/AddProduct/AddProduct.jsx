02da73d908e52d73662c6e72667825dce5fe052b
Your NameSRVDATTA1986 <tagorerabindranath1861@gmail.com>
2023-10-19 00:10:16 +0530

    Add Product form updated with onSubmit and onChange Options

--- src/components/AddProduct/AddProduct.jsx
+++ src/components/AddProduct/AddProduct.jsx
@@ -1,12 +1,33 @@
+import { useState } from "react";
 import Navbar from "../Shared/Navbar";
 
 
 const AddProduct = () => {
+    const [state,setState]=useState("Sony");
+    const handleChange = event=>{
+        const brand=event.target.value;
+        setState(brand);
+        console.log(brand);
+    }
+    const handleAddProduct=e=>{
+        e.preventDefault();
+        const form=e.target;
+        const image=form.image.value;
+        const name=form.name.value;
+        const type=form.type.value;
+        const price=form.price.value;
+        const description=form.description.value;
+        const rating=form.rating.value;
+        
+        console.log(image,name,state,type,price,description,rating);
+        form.reset();
+    }
+
     return (
         <div>
             <Navbar></Navbar>
 
-            <form>
+            <form onSubmit={handleAddProduct}>
                 <div className="relative z-0 w-full mb-6 group">
                     <input type="url" name="image" id="Image URL" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="Image URL" required />
 
@@ -18,12 +39,12 @@ const AddProduct = () => {
                 <div className="relative z-0 w-full mb-6 group">
 
                     <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Brand Name</label>
-                    <select id="countries" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                        <option>Sony</option>
-                        <option>Samsung</option>
-                        <option>LG</option>
-                        <option>Apple</option>
-                        <option>Dell</option>
+                    <select onChange={handleChange} id="countries" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                        <option value="Sony">Sony</option>
+                        <option value="Samsung">Samsung</option>
+                        <option value="LG">LG</option>
+                        <option value="Apple">Apple</option>
+                        <option value="Dell">Dell</option>
                         
                     </select>
 
--- src/components/BrandDetails/BrandDetails.jsx
+++ src/components/BrandDetails/BrandDetails.jsx
@@ -1,10 +1,29 @@
 import Navbar from "../Shared/Navbar";
-
+import AwesomeSlider from 'react-awesome-slider';
+import 'react-awesome-slider/dist/styles.css';
+import withAutoplay from 'react-awesome-slider/dist/autoplay';
+import logo1 from '../../assets/logo1.jpg'
+import logo2 from '../../assets/logo2.jpg'
+import logo3 from '../../assets/logo3.jpg'
 
 const BrandDetails = () => {
+    const AutoplaySlider = withAutoplay(AwesomeSlider)
     return (
         <div>
             <Navbar></Navbar>
+            <div className='max-w-6xl'>
+
+            <AutoplaySlider animation="cubeAnimation" play={true}
+                    cancelOnInteraction={false}
+                    interval={500} >
+
+                    <div data-src={logo1}/>
+                    <div data-src={logo2} />
+                    <div data-src={logo3} />
+                 
+                </AutoplaySlider>
+            
+        </div>
         </div>
     );
 };
